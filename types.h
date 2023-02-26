22807b82be34d35bc6abe35a97f8d01fbabf20c6
Lingkang <karlzhu12@gmail.com>
2023-02-26 21:28:39 +0800

    rename types

--- types.h
+++ types.h
@@ -1,9 +1,9 @@
 #ifndef __TYPES_H__
 #define __TYPES_H__
 
-typedef unsigned char uint8_t;
-typedef unsigned short uint16_t;
-typedef unsigned int uint32_t;
-typedef unsigned long long uint64_t;
+typedef unsigned char uint8;
+typedef unsigned short uint16;
+typedef unsigned int uint32;
+typedef unsigned long long uint64;
 
 #endif
