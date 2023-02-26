800c8efa5ca5c964e5f588e17cecaf4db37a1247
Lingkang <karlzhu12@gmail.com>
2023-02-26 21:37:21 +0800

    read and write uart registers

--- uart.c
+++ uart.c
@@ -2,19 +2,36 @@
 
 #define UART0 0x10000000 // memory address of UART0
 
-// Register bit maps of UART
+// UART control registers
+// defined only when needed
 #define RHR 0
 #define THR 0
-#define IER 1
+#define DLL 0 // Divisor Latch Least Significant Bit (WRITE)
+#define DLM 1 // Divisor Latch Most Significant Bit (WRITE)
+#define IER 1 // Interrupt Enable Register (WRITE)
 #define CFR 2
 #define ISR 2
-#define LCR 3
+#define LCR 3 // Line Control Register (WRITE)
 #define MCR 4
 #define LSR 5
 #define MSR 6
 #define SPR 7
 
-volatile uint8_t get_reg(int reg);
+volatile uint8 *get_reg(int reg) {
+    // return the memory location of given register name
+    return (volatile uint8 *)(reg + UART0);
+}
+
+void uart_write_reg(int reg, uint8 val) {
+    volatile uint8 *p = get_reg(reg);
+    *p = val;
+    return;
+}
+
+uint8 uart_read_reg(int reg) {
+    return *(get_reg(reg));
+}
+
 void uart_init(void);
 void uart_putchar(char c);
 void uart_puts(char *s);
