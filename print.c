08ceeb14b3f86463336c44f1aa48f10cb7777168
Lingkang <karlzhu12@gmail.com>
2023-03-03 09:28:47 +0800

    Print hex value of pointer

--- print.c
+++ print.c
@@ -4,6 +4,34 @@
 
 #define TODO(void) (panic("TODO"))
 
+void kprintint(uint64 i)
+{
+    TODO();
+}
+
+char digits[] = "0123456789ABCDEF";
+
+void kprintptr(uint32 p)
+{
+    // print hex value of a pointer.
+    uint8 digit;
+    uint8 i = 7;
+    uint32 four_bits;
+    uart_puts("0x");
+    while (1)
+    {
+        four_bits = p & (15 << i * 4);
+        digit = four_bits >> i * 4;
+        uart_putchar(digits[digit]);
+        if (i == 0)
+        {
+            break;
+        }
+        i--;
+    }
+    return;
+}
+
 void kprintf()
 {
     TODO();
@@ -16,4 +44,20 @@ void panic(char *s)
     uart_puts("\n");
     while (1)
         ;
+    return;
+}
+
+void test_print(void)
+{
+    // kprintptr test
+    kprintptr(2);
+    uart_putchar('\n');
+    kprintptr(15);
+    uart_putchar('\n');
+    kprintptr(16);
+    uart_putchar('\n');
+    kprintptr(123456789); // 075B CD15
+    uart_putchar('\n');
+    kprintptr(4294967295); // FFFF FFFF
+    uart_putchar('\n');
 }
