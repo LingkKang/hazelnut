2a01a32e4c315ed3f62a4df13df3d58524ab28a8
Lingkang <karlzhu12@gmail.com>
2023-04-15 18:16:51 +0800

    Write value to `mtimecmp` register

--- timer.s
+++ timer.s
@@ -1,4 +1,48 @@
-# RISC-V 32 sleep function
+.text
+
+.equ            CLINT, 0x2000000
+.equ            CLINT_MTIME, CLINT + 0xBFF8
+.equ            CLINT_MTIMECMP, CLINT + 0x4000
+
+# void timer_load(int delta);
+
+.func timer_load
+.globl timer_load
+
+timer_load:
+    # set value in mtimercmp register
+
+    # get current time
+    li          t0, CLINT_MTIME         # t0 is address of mtime (lower bits)
+    lw          s0, 0(t0)               # value of current time (lower bits)
+    li          t0, CLINT_MTIME
+    lw          s1, 4(t0)               # value of current time (higher bits)
+
+    # get value to store at mtimecmp
+    # value is mtime + delta
+    mv          t0, s0
+    add         s0, s0, a0              # value should be set to mtimecmp
+    sltu        t1, s0, t0
+    add         s1, s1, t1
+
+    # save value to the stack
+    addi        sp, sp, -4
+    sw          ra, 0(sp)
+
+    # get mhartid
+    call        read_mhartid            # hartid saved at a0
+
+    # find address of mtimecmp
+    slli        a0, a0, 3               # hartid * 8
+    li          t0, CLINT_MTIMECMP
+    add         a0, a0, t0
+    sw          s0, 0(a0)
+    sw          s1, 4(a0)
+    lw          ra, 0(a0)
+    addi        sp, sp, 4
+
+    ret
+.endfunc
 
 # void ksleep_millisec(unsigned int t);
 
