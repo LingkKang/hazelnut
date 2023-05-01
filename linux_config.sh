dbb53ae0d34d29cb71f055fdafe588dc5b44368a
Lingkang <karlzhu12@gmail.com>
2023-05-01 15:05:03 +0800

    Update linux_config.sh

--- linux_config.sh
+++ linux_config.sh
@@ -4,6 +4,8 @@ sudo git clone https://github.com/riscv/riscv-gnu-toolchain
 
 sudo apt-get install autoconf automake autotools-dev curl python3 libmpc-dev libmpfr-dev libgmp-dev gawk build-essential bison flex texinfo gperf libtool patchutils bc zlib1g-dev libexpat-dev ninja-build
 
+sudo apt install tree
+
 cd riscv-gnu-toolchain
 
 sudo ./configure --prefix=/opt/riscv
@@ -11,5 +13,7 @@ sudo ./configure --prefix=/opt/riscv
 sudo make linux
 
 export PATH=$PATH:.
+
+tree
 
 cd ..
