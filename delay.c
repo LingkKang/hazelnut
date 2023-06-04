ecd8b619c4c508dcbfa5cd05942115c4d1b19710
Lingkang <karlzhu12@gmail.com>
2023-06-04 19:02:42 +0800

    Fix bug `delay`

--- delay.c
+++ delay.c
@@ -2,12 +2,15 @@
 
 #define DELAY 3686400 * 2 // 1 sec or so
 
+/*
+Delay the thread for given time which measured in secondes,
+not accurate.
+*/
 void delay(int time)
 {
-    // TODO();
     uint32 time_in_hz = time * DELAY;
-    uint32 finish_time = time_in_hz + sys_get_hw_time();
-    while (finish_time <= sys_get_hw_time())
+    uint32 start_time = sys_get_hw_time();
+    while (sys_get_hw_time() - start_time <= time_in_hz)
         ;
     return;
 }
